f6b8aa1052486022ff1d4c33de383f1ecaa128a5
RealTun <maumai07@gmail.com>
2024-12-19 23:15:06 +0700

    predict job, api run exe py

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { searchJobs, findCompanyByName } = require('../controllers/api.controller');
+const { searchJobs, findCompanyByName, predictCareer } = require('../controllers/job.controller');
 const { login, getSummaryMark } = require('../controllers/student.controller');
 const router = express.Router();
 
@@ -10,5 +10,8 @@ router.get('/student/getSummaryMark', getSummaryMark);
 // jobs
 router.post('/jobs/search', searchJobs);
 router.post('/company/search', findCompanyByName);
+
+// predict
+router.post('/predict/career', predictCareer);
 
 module.exports = router;
